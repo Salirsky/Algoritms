ca7630d72fa902fcda2af0cc65fa0523e1dbf1ea
Marina Belova <83077065+Salirsky@users.noreply.github.com>
2021-08-16 01:18:58 +0300

    add code binary search

--- binary_search.py
+++ binary_search.py
@@ -1,1 +1,17 @@
-
+  def binary_search(list, item):
+    low = 0
+    high - len(list)-1 #Переменные low и high содержат границы той части списка, в которой выполняется поиск.
+    while low <= high:
+      mid = (low + high)/2 #mid - средний элемент
+      guess = list[mid]
+      if guess == item:
+        return mid
+      if guess > item:
+        high = mid - 1
+        else:
+          low - mid + 1
+    return None
+  
+  my_list = [1, 3, 5, 7, 9]
+  
+  
